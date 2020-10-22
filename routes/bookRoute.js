00c944ca296b7d0bf8941372062345e821c030c4
torianyap <torianyap@gmail.com>
2020-10-22 21:41:59 +0700

    lalala

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -2,9 +2,12 @@ const route = require('express').Router()
 const BookController = require('../controllers/book')
 
 route.get('/', BookController.listBooks)
+
 route.get('/add', BookController.addBook)
 route.post('/add', BookController.addBookPost)
+
 route.get('/:id/delete', BookController.delete)
+
 route.get('/:id/edit', BookController.editForm)
 route.post('/:id/edit', BookController.edit)
 
